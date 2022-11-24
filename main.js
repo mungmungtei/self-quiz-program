ff71942d4af1c320bccafdfa2c6296be8dc66249
Joohee Kim <mungmungtei@gmail.com>
2022-11-24 15:59:07 +0900

    feat: show input value on browser when press enter key

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
-const quizList = document.querySelector(".quizList");
-const quiz = document.createElement("h2");
-quiz.setAttribute("class", "quiz");
-quiz.textContent = "여기에 사용자의 입력값이 들어갑니다";
-quizList.append(quiz);
+function addQuiz(event) {
+  const inputQuiz = document.querySelector(".inputQuiz");
+  const quizContent = inputQuiz.value;
+  const quiz = document.createElement("h2");
+  quiz.setAttribute("class", "quiz");
+  if (event.key == "Enter" && quizContent) {
+    quiz.textContent = quizContent;
+    document.querySelector(".quizList").appendChild(quiz);
+    window.scrollTo(0, document.body.scrollHeight); // 콘텐츠가 길이에 맞춰 아래로 스크롤되도록
+    console.log(quizContent); // 나중에 지우기
+    inputQuiz.value = null; // input 입력 완료 시, 값 초기화시키도록
+  }
+}
+
+window.addEventListener("keypress", addQuiz);
