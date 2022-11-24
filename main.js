50e96a9232f107fa01dc063823259c25203b8f02
Joohee Kim <mungmungtei@gmail.com>
2022-11-24 10:37:05 +0900

    feat: html markup, css styling

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-console.log("connected!!");
+const quizList = document.querySelector(".quizList");
+const quiz = document.createElement("h2");
+quiz.setAttribute("class", "quiz");
+quiz.textContent = "여기에 사용자의 입력값이 들어갑니다";
+quizList.append(quiz);
