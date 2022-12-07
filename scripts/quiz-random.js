ef80eb7722939ed1271beffe594dd03624722aa1
Joohee Kim <mungmungtei@gmail.com>
2022-12-07 21:31:29 +0900

    feat: update local storage when click answer button

--- scripts/quiz-random.js
+++ scripts/quiz-random.js
@@ -1,5 +1,6 @@
 "use strict";
 
+// 다음 버튼 클릭 시, 새로운 퀴즈 보여주기
 const singleQuiz = document.querySelector(".singleQuiz");
 const nextBtn = document.querySelector(".nextBtn");
 
@@ -39,3 +40,27 @@ const renderSingleQuiz = (randomArray) => {
 
 const randomArray = sortByRandom();
 renderSingleQuiz(randomArray);
+
+// 답변에 대한 버튼 클릭 시, local storage 업데이트 하기
+const btns = document.querySelector(".btns");
+
+const updateStatus = (event) => {
+  const btn = event.target;
+  const text = singleQuiz.textContent;
+  const quizList = JSON.parse(localStorage.getItem("quizList"));
+  const quiz = quizList.find((item) => item.content === text);
+
+  if (btn.classList.contains("circle")) {
+    quiz.status = "circle";
+  }
+  if (btn.classList.contains("triangle")) {
+    quiz.status = "triangle";
+  }
+  if (btn.classList.contains("letterX")) {
+    quiz.status = "letterX";
+  }
+
+  localStorage.setItem("quizList", JSON.stringify(quizList));
+};
+
+btns.addEventListener("focus", updateStatus, true);
